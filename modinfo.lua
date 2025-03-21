7b427c96bead71554c3975efd25cdb62a4fd6cfb
alicia-vanca <trinhtkhanhvan@gmail.com>
2025-03-21 09:41:42 +0700

    1.1.9_01
    For Wormwood and other mod chars with similar ability:
    
    - Added intercropping support for bare-handed planting: Automatically switches tiles after planting a configured number of seeds.
    
    - Added an option in mod configs to toggle the Intercropping mode (default: " ; " button).
    
    - Show intercropping mode when picking up seeds as Wormwood (snapping mode On is required).
    
    - Auto pickup a new stack of seeds when planting.
    
    - Controller support added: Untested with physical controllers – please report any issues!

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,7 @@
-version = "1.1.9_00"
+version = "1.1.9_01"
 name = "Snapping tills A WHOLE FIELD v" .. version
-description = [[Aligns plowing to the grid.
+description =
+    [[Aligns plowing to the grid.
 
 LeftShift + RightClick: Launch auto tilling on a tile.
 LeftShift + DoubleRightClick: Launch auto tilling on multi tiles.
@@ -28,45 +29,143 @@ dst_compatible = true
 all_clients_require_mod = false
 client_only_mod = true
 
-local key_list = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","TAB","CAPSLOCK","LSHIFT","RSHIFT","LCTRL","RCTRL","LALT","RALT","ALT","CTRL","SHIFT","SPACE","ENTER","ESCAPE","MINUS","EQUALS","BACKSPACE","PERIOD","SLASH","LEFTBRACKET","BACKSLASH","RIGHTBRACKET","TILDE","PRINT","SCROLLOCK","PAUSE","INSERT","HOME","DELETE","END","PAGEUP","PAGEDOWN","UP","DOWN","LEFT","RIGHT","KP_DIVIDE","KP_MULTIPLY","KP_PLUS","KP_MINUS","KP_ENTER","KP_PERIOD","KP_EQUALS"}
+local key_list = {
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "F1",
+    "F2",
+    "F3",
+    "F4",
+    "F5",
+    "F6",
+    "F7",
+    "F8",
+    "F9",
+    "F10",
+    "F11",
+    "F12",
+    "TAB",
+    "CAPSLOCK",
+    "LSHIFT",
+    "RSHIFT",
+    "LCTRL",
+    "RCTRL",
+    "LALT",
+    "RALT",
+    "ALT",
+    "CTRL",
+    "SHIFT",
+    "SPACE",
+    "ENTER",
+    "ESCAPE",
+    "MINUS",
+    "EQUALS",
+    "BACKSPACE",
+    "PERIOD",
+    "SLASH",
+    "SEMICOLON",
+    "LEFTBRACKET",
+    "BACKSLASH",
+    "RIGHTBRACKET",
+    "TILDE",
+    "PRINT",
+    "SCROLLOCK",
+    "PAUSE",
+    "INSERT",
+    "HOME",
+    "DELETE",
+    "END",
+    "PAGEUP",
+    "PAGEDOWN",
+    "UP",
+    "DOWN",
+    "LEFT",
+    "RIGHT",
+    "KP_DIVIDE",
+    "KP_MULTIPLY",
+    "KP_PLUS",
+    "KP_MINUS",
+    "KP_ENTER",
+    "KP_PERIOD",
+    "KP_EQUALS"
+}
 local key_options = {}
 
 for i = 1, #key_list do
-    key_options[i] = { description = key_list[i], data = "KEY_"..key_list[i] }
+    key_options[i] = {description = key_list[i], data = "KEY_" .. key_list[i]}
 end
 
-configuration_options =
-{
+configuration_options = {
     {
         name = "language",
         label = "Language",
         hover = "Default language.",
-        options =
-        {
-            {description = "Auto detect",  data = "auto",  hover = "Auto detect" },
-            {description = "English",  data = "en",  hover = "English" },
-            {description = "Español",  data = "esp", hover = "Español"},
-            {description = "简体中文",   data = "sch", hover = "Simplified Chinese"},
-            {description = "Русский",  data = "ru",  hover = "Russian"},
+        options = {
+            {description = "Auto detect", data = "auto", hover = "Auto detect"},
+            {description = "English", data = "en", hover = "English"},
+            {description = "Español", data = "esp", hover = "Español"},
+            {description = "简体中文", data = "sch", hover = "Simplified Chinese"},
+            {description = "Русский", data = "ru", hover = "Russian"}
         },
-        default = "auto",
+        default = "auto"
     },
     {
         name = "visiblesnaps",
         label = "Visible snaps",
         hover = "Visible snaps",
-        options =
-        {
+        options = {
             {description = "on", data = true},
-            {description = "off", data = false},
+            {description = "off", data = false}
         },
-        default = true,
+        default = true
     },
     {
         name = "keychagemode",
         label = "Toggle snap mode",
         hover = "Key to toggle snap mode",
         options = key_options,
-        default = "KEY_L",
+        default = "KEY_L"
+    },
+    {
+        -- 250320 VanCa: Add key to change intercropping mode (1/2/3/4 types of plants on 1 tile)
+        name = "key_chage_intercropping_mode",
+        label = "Toggle intercropping mode",
+        hover = "Key to toggle intercropping mode",
+        options = key_options,
+        default = "KEY_SEMICOLON"
     }
 }
